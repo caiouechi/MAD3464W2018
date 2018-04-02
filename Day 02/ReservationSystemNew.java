cedded5cd6105a35eb2caf460bec9dac41d2edc8
caiouechi <caio_uechi@hotmail.com>
2018-04-02 10:54:30 -0400

    Add files via upload

---
+++ Day 02/ReservationSystemNew.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day2;
+
+/**
+ *
+ * @author droidpulkit
+ */
+public class ReservationSystemNew {
+    public static void main(String[] args) {
+        AirlineReservation obj1 = new AirlineReservation();
+        obj1.book();
+    }
+}
